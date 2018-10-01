8163b5662c84578783740c06d587efc40af1d724
Raymond Kwan <rayk199492@gmail.com>
2018-10-01 21:24:48 +0000

    Created Basic Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,57 @@
 import React, {Component} from 'react';
+import { Chatbar, Navbar } from './ChatBar.jsx';
+import MessageList from './MessageList.jsx'
+// import data from './data.json';
+
+const data = [
+  {
+    type: "incomingMessage",
+    content: "I won't be impressed with technology until I can download food.",
+    username: "Anonymous1"
+  },
+  {
+    type: "incomingNotification",
+    content: "Anonymous1 changed their name to nomnom",
+  },
+  {
+    type: "incomingMessage",
+    content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
+    username: "Anonymous2"
+  },
+  {
+    type: "incomingMessage",
+    content: "...",
+    username: "nomnom"
+  },
+  {
+    type: "incomingMessage",
+    content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
+    username: "Anonymous2"
+  },
+  {
+    type: "incomingMessage",
+    content: "This isn't funny. You're not funny",
+    username: "nomnom"
+  },
+  {
+    type: "incomingNotification",
+    content: "Anonymous2 changed their name to NotFunny",
+  },
+]
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { data };
+  }
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <h1>Hello React :)</h1>
+        <Navbar />
+        <MessageList data={data} />
+        <Chatbar />
+      </div>
     );
   }
 }
